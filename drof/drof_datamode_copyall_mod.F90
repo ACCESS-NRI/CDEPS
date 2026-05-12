1e607031c45a6d8e5f201c63759c79ea96c42b2c
Dougie Squire <dougie.squire@anu.edu.au>
2026-05-12 11:30:26 +1000

    drof: make Forr_rofl stream optional (#6)

--- drof/drof_datamode_copyall_mod.F90
+++ drof/drof_datamode_copyall_mod.F90
@@ -7,6 +7,7 @@ module drof_datamode_copyall_mod
   use dshr_methods_mod , only : dshr_state_getfldptr, chkerr
   use dshr_strdata_mod , only : shr_strdata_type, shr_strdata_get_stream_pointer
   use shr_const_mod    , only : SHR_CONST_SPVAL
+  use shr_log_mod      , only : shr_log_error
 
   implicit none
   private
@@ -20,8 +21,8 @@ module drof_datamode_copyall_mod
   real(r8), pointer :: Forr_rofi(:) => null()
 
   ! stream pointer arrays
-  real(r8), pointer :: strm_Forr_rofl(:) => null() ! always required
-  real(r8), pointer :: strm_Forr_rofi(:) => null() ! sometimes present in stream
+  real(r8), pointer :: strm_Forr_rofl(:) => null() ! optional, but at least one provided in stream
+  real(r8), pointer :: strm_Forr_rofi(:) => null() ! optional, but at least one provided in stream
 
   character(len=*) , parameter :: u_FILE_u = &
        __FILE__
@@ -79,15 +80,21 @@ contains
     call dshr_state_getfldptr(exportState, 'Forr_rofi' , fldptr1=Forr_rofi , rc=rc)
     if (chkerr(rc,__LINE__,u_FILE_u)) return
 
-    ! Initialize module pointers
-    call shr_strdata_get_stream_pointer( sdat, 'Forr_rofl', strm_Forr_rofl, requirePointer=.true., &
-         errmsg=subname//'ERROR: strm_Forr_rofl must be associated for drof', rc=rc)
+    call shr_strdata_get_stream_pointer( sdat, 'Forr_rofl', strm_Forr_rofl, rc=rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
+    if (.not. associated(strm_Forr_rofl)) then
+       Forr_rofl(:) = 0._r8
+    end if
 
     call shr_strdata_get_stream_pointer( sdat, 'Forr_rofi', strm_Forr_rofi, rc=rc)
     if (ChkErr(rc,__LINE__,u_FILE_u)) return
     if (.not. associated(strm_Forr_rofi)) then
        Forr_rofi(:) = 0._r8
+    end if
+
+    if (.not. associated(strm_Forr_rofl) .and. .not. associated(strm_Forr_rofi)) then
+       call shr_log_error(subname//'ERROR: At least one of strm_Forr_rofl or strm_Forr_rofi must be associated for drof', rc=rc)
+       return
     end if
 
   end subroutine drof_datamode_copyall_init_pointers
@@ -100,13 +107,15 @@ contains
     !-------------------------------------------------------------------------------
 
     ! zero out "special values" of export fields
-    do ni = 1, size(Forr_rofl)
-       if (abs(strm_Forr_rofl(ni)) < 1.e28_r8) then
-          Forr_rofl(ni) = strm_Forr_rofl(ni)
-       else
-          Forr_rofl(ni) = 0.0_r8
-       end if
-    enddo
+    if (associated(strm_Forr_rofl)) then
+       do ni = 1, size(Forr_rofl)
+          if (abs(strm_Forr_rofl(ni)) < 1.e28_r8) then
+             Forr_rofl(ni) = strm_Forr_rofl(ni)
+          else
+             Forr_rofl(ni) = 0.0_r8
+          end if
+       enddo
+    end if
 
     if (associated(strm_Forr_rofi)) then
        do ni = 1, size(Forr_rofi)
